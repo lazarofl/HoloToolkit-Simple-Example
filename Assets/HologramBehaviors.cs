e419ab9f667cf522d1248cbf7e6a463692aef6b6
Lazaro Fernandes Lima Suleiman <lazaro.fl@gmail.com>
2017-02-02 00:07:46 -0200

    First commit

---
+++ Assets/HologramBehaviors.cs
@@ -0,0 +1,42 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class HologramBehaviors : MonoBehaviour, IFocusable, IInputClickHandler
+{
+    [SerializeField]
+    public bool IsRotating = false;
+
+    [SerializeField]
+    public Color FocusedColor = Color.red;
+
+    private Color OriginalColor;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.OriginalColor = gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsRotating)
+            gameObject.transform.Rotate(1, 1, 1);
+    }
+
+    public void OnFocusEnter()
+    {
+        gameObject.GetComponent<Renderer>().material.color = FocusedColor;
+    }
+
+    public void OnFocusExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = OriginalColor;
+    }
+
+    public void OnInputClicked(InputEventData eventData)
+    {
+        IsRotating = !IsRotating;
+    }
+
+}
